0b35117a85092516aabf761cf0899d89256d1894
Kamlesh Kumar Saini <131683941+BrothersIndia7@users.noreply.github.com>
2023-08-29 09:55:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -58,7 +58,8 @@ logging = logging.getLogger()
 bot = Client("bot",
              bot_token=Config.BOT_TOKEN,
              api_id=Config.API_ID,
-             api_hash=Config.API_HASH
+             api_hash=Config.API_HASH, 
+             worker=6
             )
 
 auth_users=Config.AUTH_USERS
